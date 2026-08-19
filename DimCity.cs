78963a623bffa5035f566380850b1be1555723af
Tako Lansbergen <takolansbergen@xs4all.nl>
2026-08-19 21:54:04 +0200

    Tilin'

--- DimCity.cs
+++ DimCity.cs
@@ -9,7 +9,9 @@ internal class DimCity
     {
         for (int i = 5; i < 10; i++)
         {
+            // temporarily make some water
             Tiles.Add(new Vector2(i, 10), new Tile { Terra = BuildingType.Water });
+            Tiles.Add(new Vector2(i+3, 11), new Tile { Terra = BuildingType.Water });
         }
     }
 }
